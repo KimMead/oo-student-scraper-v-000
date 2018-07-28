460afa67ee03fe324b24a6237380a6a45e4f4546
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-07-28 22:45:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,10 +19,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each
-
-
-  end
+    students_array.each do |student|
+      puts "#{person[:name]}: #{person[:occupation]}"
+    end 
+  end 
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
